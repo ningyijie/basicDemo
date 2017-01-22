7b552be60b548a389e607f2dbb9da6ae01c93ad0
ningyijie <ningyijie@puhuifinance.com>
2017-01-22 20:06:01 +0800

    groupby

---
+++ src/main/java/com/example/java8/Java8Example1.java
@@ -0,0 +1,33 @@
+package com.example.java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Created by cnbjpuhui-384 on 2017/1/22.
+ * Group by a List and display the total count of it.
+ */
+public class Java8Example1 {
+    public static void main(String[] args) {
+
+        //3 apple, 2 banana, others 1
+        List<String> items =
+                Arrays.asList("apple", "apple", "banana",
+                        "apple", "orange", "banana", "papaya");
+
+        Map<String, Long> result =
+                items.stream().collect(
+                        Collectors.groupingBy(
+                                Function.identity(), Collectors.counting()
+                        )
+                );
+
+        System.out.println(result);
+
+
+    }
+}
+
