56d027876978e574388ad89254aa1a81be21772b
ningyijie <ningyijie@puhuifinance.com>
2017-07-12 14:03:11 +0800

    Lambda 表达式基础语法1

---
+++ src/main/java/com/example/java8/atguigu/TestLambda.java
@@ -0,0 +1,8 @@
+package com.example.java8.atguigu;
+
+/**
+ * Desc:
+ * Created by ningyijie on 2017/7/12.
+ */
+public class TestLambda {
+}
---
+++ src/main/java/com/example/java8/function/LambdaBasic1.java
@@ -0,0 +1,8 @@
+package com.example.java8.function;
+
+/**
+ * Desc:
+ * Created by ningyijie on 2017/7/12.
+ */
+public class LambdaBasic1 {
+}
