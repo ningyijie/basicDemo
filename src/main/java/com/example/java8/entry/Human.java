ad6266f2b0550aadd595fec6da09c7122f1789fa
ningyijie <ningyijie@finupgroup.com>
2017-10-20 14:35:50 +0800

    增加排序

--- src/main/java/com/example/java8/entry/Human.java
+++ src/main/java/com/example/java8/entry/Human.java
@@ -1,7 +1,40 @@
 package com.example.java8.entry;
 
 /**
+ * Desc:排序用到的实体
  * Created by ningyijie on 2017/7/25.
  */
 public class Human {
+
+    public String name;
+    public int age;
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public static int compareByNameThenAge(Human lhs, Human rhs) {
+        if (lhs.name.equals(rhs.name)) {
+            return lhs.age - rhs.age;
+        } else {
+            return lhs.name.compareTo(rhs.name);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
--- src/main/java/com/example/java8/function/Sort.java
+++ src/main/java/com/example/java8/function/Sort.java
@@ -1,7 +1,50 @@
 package com.example.java8.function;
 
+import com.example.java8.entry.Human;
+import com.google.common.collect.Lists;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+
 /**
+ * Desc:java 8 排序
  * Created by ningyijie on 2017/7/25.
  */
 public class Sort {
+
+    @Test
+    public void whenSortingentitiesByName() {
+        List<Human> humans = Lists.newArrayList(new Human("Sarah", 10), new Human("Jack", 12));
+        humans.sort(( h1,  h2) -> h1.getName().compareTo(h2.getName()));
+        Assert.assertThat(humans.get(0).getAge(), equalTo(12));
+    }
+
+
+    /**
+     *
+     */
+    @Test
+    public void givenMethodDefinition_whenSortingEntitiesByNameThenAge_thenCorrectlySorted() {
+        List<Human> humans = Lists.newArrayList(new Human("Sarah", 10), new Human("Jack", 12));
+        humans.sort(Human::compareByNameThenAge);
+        Assert.assertThat(humans.get(0).getAge(), equalTo(12));
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void givenInstanceMethod_whenSortingEntitiesByNameThenAge_thenCorrectlySorted() {
+        List<Human> humans = Lists.newArrayList(new Human("Sarah", 10), new Human("Jack", 12));
+        Collections.sort(humans, Comparator.comparing(Human::getName));
+        Assert.assertThat(humans.get(0).getAge(), equalTo(12));
+    }
+
+
+
 }
