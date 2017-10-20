50378aed1f4e27e699b5f1c10ce0fb9721e40668
ningyijie <ningyijie@finupgroup.com>
2017-10-20 14:33:11 +0800

    修改

---
+++ src/main/java/com/example/java8/entry/Human.java
@@ -0,0 +1,7 @@
+package com.example.java8.entry;
+
+/**
+ * Created by ningyijie on 2017/7/25.
+ */
+public class Human {
+}
---
+++ src/main/java/com/example/java8/function/Sort.java
@@ -0,0 +1,7 @@
+package com.example.java8.function;
+
+/**
+ * Created by ningyijie on 2017/7/25.
+ */
+public class Sort {
+}
